c4b9e26394fd36ea735fd243e0192dc79c7b94bd
Daniel Soto <dasg.1993@gmail.com>
2021-06-09 20:22:38 -0500

    [feature] - create async await arrow functions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ async function requestHttp () {
     console.log(result);
 }
 
-requestHttp();
+// requestHttp();
 
 async function showAvatar() {
     // read github user
@@ -22,11 +22,100 @@ async function showAvatar() {
     img.className = 'promise-avatar-example';
     document.body.append(img);
 
-    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    // await new Promise((resolve, reject) => setTimeout(resolve, 3000));
 
-    img.remove();
+    // img.remove();
     console.log(githubUser)
     return githubUser;
 }
 
-showAvatar();
+// showAvatar();
+
+class Thenable {
+    constructor(num) {
+        this.num = num
+    }
+    then(resolve, reject) {
+        console.log(resolve);
+        // resolve with this.num*2 after 1500ms
+        setTimeout(() => resolve(this.num * 2) , 1500)
+    }
+}
+
+async function response() {
+    // waits for 1.5 seconds, then result becomes
+    let result = await new Thenable(1);
+    console.log(result)
+}
+
+// response();
+
+class Waiter {
+    async wait() {
+        return await Promise.resolve(1)
+    }
+}
+
+new Waiter()
+    .wait()
+    .then((response) => console.log(response));
+
+// async function errorMessage() {
+//     await Promise.reject(new Error("Whooops!"))
+// }
+
+
+async function errorMessage() {
+    try {
+        let products = await fetch('https://no-such-url');
+        // let response = await fetch('https://no-such-url/order', {
+        //     method: 'POST',
+        //     body: products
+        // });
+
+        // let resuts = await Promise.all([
+        //     products,
+        //     response
+        // ])
+
+        return products;
+    } catch(err) {
+        console.log('error fetch', err)
+    }
+}
+
+// errorMessage();
+
+const who = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('🐶')
+        }, 200)
+    })
+}
+
+const what = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('walks in')
+        }, 300)
+    })
+}
+
+const where = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('🏞')
+        })
+    })
+}
+
+const message = async () => {
+    const a = await who();
+    const b = await what();
+    const c = await where();
+
+    console.log(`${a} ${b} ${c}`)
+}
+
+message();
